c62bbc4e90922843a09800b267510560b01b7b12
RubenF0nseca <153688053+RubenF0nseca@users.noreply.github.com>
2024-12-18 18:46:11 +0000

    add n-null and 'Idade_Milissegundos'

--- project_1.py
+++ project_1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import datetime
 
 df = pd.read_csv('titanic.csv')
 
@@ -18,5 +18,22 @@ print(df.info())
 print("-"*150)
 
 print(df.describe())
+
+# tratamento de dados
+df['Age'] = df['Age'].fillna(0) # Preencher valores nulos de 'Age' com 0
+df['Cabin'] = df['Cabin'].fillna('N/A') # Preencher valores nulos de 'Cabin' com "N/A"
+df['Fare'] = df['Fare'].fillna(0) # Preencher valores nulos de 'Fare' com 0
+
+def calcular_milissegundos(idade):
+    if idade == 0:
+        return 0
+    nascimento = datetime.datetime(1970, 1, 1, tzinfo=datetime.timezone.utc) - datetime.timedelta(days=idade * 365.25)
+    epoch = datetime.datetime(1970, 1, 1, tzinfo=datetime.timezone.utc)
+    return int((nascimento - epoch).total_seconds() * 1000)
+
+df['Idade_Milissegundos'] = df['Age'].apply(calcular_milissegundos)
+
+print(df.head())
+
 
 
